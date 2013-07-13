f42ebb668d01e82d5e59c4ce508fe53b8a673854
sunghwan <sunghwanjo@nhnnext.org>
2013-07-13 14:58:05 +0900

    add Test Method Rules

--- src/one/lesson/PawnTest.java
+++ src/one/lesson/PawnTest.java
@@ -3,6 +3,13 @@ package one.lesson;
 import junit.framework.TestCase;
 
 public class PawnTest extends TestCase{
+	/*
+	 * Test Method 규칙
+	 * 1. 메소드는 public으로 선언되어야 한다
+	 * 2. 메소드는 값을 반환하지 않는다 (void)
+	 * 3. 메소드의 이름은 소문자 test로 시작
+	 * 4. 인수를 받지 않는다
+	 */
 	public void testCreate(){
 		Pawn pawn = new Pawn();
 	}
